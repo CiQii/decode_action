453ab14b7fbfb86468f842f683723635d0807f1a
action <action@github.com>
2025-01-19 14:07:58 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,22 +1,21 @@
-//Sun Jan 19 2025 14:04:53 GMT+0000 (Coordinated Universal Time)
+//Sun Jan 19 2025 14:07:58 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("GOGOGOGO");
 let url = $request.url,
   headers = $request.headers;
-url = url.replace(/\/\/(?!long)[^\.]+\./, "//long.").replace(/\.m3u8/, ".m3u8");
-if (headers.hasOwnProperty("x-playback-session-id") || headers.hasOwnProperty("X-Playback-Session-Id")) try {
+if (headers.hasOwnProperty("X-Playback-Session-Id") || headers.hasOwnProperty("x-playback-session-id")) try {
   const notify = $.getdata("m3u8");
   if (!notify || notify != url) {
     $.setdata(url, "m3u8");
-    const senPlayerUrl = "alohabrowser://open?link=" + encodeURIComponent(url),
+    const senPlayerUrl = "SenPlayer://x-callback-url/play?url=" + encodeURIComponent(url),
       mediaUrl = "https://raw.githubusercontent.com/Yu9191/-/main/dingdangmao.jpg";
-    $.msg("av获取成功", "请安装Aloha浏览器 ", "只适配此软件请前往频道安装", {
+    $.msg("获取成功", "请安装SenPlayer播放器", "已安装请忽略", {
       "open-url": senPlayerUrl,
       "media-url": mediaUrl
     });
   }
-} catch (_0x4fb4f9) {
-  console.error("An error occurred:", _0x4fb4f9);
+} catch (lliiII1I) {
+  console.error("An error occurred:", lliiII1I);
 }
 $.done({});
