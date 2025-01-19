d252539dd884fd72c98d1bb5310d9ba4bcbae216
action <action@github.com>
2025-01-19 14:12:14 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,21 +1,23 @@
-//Sun Jan 19 2025 14:07:58 GMT+0000 (Coordinated Universal Time)
+//Sun Jan 19 2025 14:12:14 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("GOGOGOGO");
 let url = $request.url,
   headers = $request.headers;
-if (headers.hasOwnProperty("X-Playback-Session-Id") || headers.hasOwnProperty("x-playback-session-id")) try {
-  const notify = $.getdata("m3u8");
-  if (!notify || notify != url) {
-    $.setdata(url, "m3u8");
-    const senPlayerUrl = "SenPlayer://x-callback-url/play?url=" + encodeURIComponent(url),
-      mediaUrl = "https://raw.githubusercontent.com/Yu9191/-/main/dingdangmao.jpg";
-    $.msg("获取成功", "请安装SenPlayer播放器", "已安装请忽略", {
-      "open-url": senPlayerUrl,
-      "media-url": mediaUrl
-    });
+if (headers.hasOwnProperty("X-Playback-Session-Id") || headers.hasOwnProperty("x-playback-session-id")) {
+  try {
+    const notify = $.getdata("m3u8");
+    if (!notify || notify != url) {
+      $.setdata(url, "m3u8");
+      const senPlayerUrl = "SenPlayer://x-callback-url/play?url=" + encodeURIComponent(url),
+        mediaUrl = "https://raw.githubusercontent.com/Yu9191/-/main/dingdangmao.jpg";
+      $.msg("获取成功", "请安装SenPlayer播放器", "已安装请忽略", {
+        "open-url": senPlayerUrl,
+        "media-url": mediaUrl
+      });
+    }
+  } catch (lliiII1I) {
+    console.error("An error occurred:", lliiII1I);
   }
-} catch (lliiII1I) {
-  console.error("An error occurred:", lliiII1I);
 }
 $.done({});
